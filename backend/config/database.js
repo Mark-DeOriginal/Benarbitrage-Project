91f5fc9885f8d0d787208babbef1c8f5c8a6c598
Mark Friday <davidmarkfriday16@gmail.com>
2023-12-16 21:55:59 +0100

    Made some nice changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,22 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = () => {
-  if (process.env.MYSQL_PRIVATE_URL) {
-    return new Sequelize(process.env.MYSQL_PRIVATE_URL, {
-      dialect: process.env.DB_DIALECT, // Optional if specified in the URL
-    });
-  } else {
-    return new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-      }
-    );
-  }
-};
+const db = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+});
 
-export default db();
+export default db;
--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,10 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: process.env.SERVICE_URL,
+  origin: "*",
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
