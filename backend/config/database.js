e3ede068cee68a857798741d9d91924d99185aaf
Mark Friday <davidmarkfriday16@gmail.com>
2023-12-16 19:24:50 +0100

    Made some updates to the database.js file

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,14 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+const db = () => {
+  if (process.env.MYSQL_PRIVATE_URL) {
+    return new Sequelize(process.env.MYSQL_PRIVATE_URL, {
+      dialect: process.env.DB_DIALECT, // Optional if specified in the URL
+    });
+  } else {
+    return new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+      }
+    );
   }
-);
+};
 
-export default db;
+export default db();
