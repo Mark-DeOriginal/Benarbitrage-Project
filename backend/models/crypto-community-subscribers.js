7b7a219cd1ea7ffcc4908b9000bfa80c646430b5
Mark Friday <davidmarkfriday16@gmail.com>
2024-01-02 21:30:30 +0100

    Made some corrections

--- backend/models/crypto-community-subscribers.js
+++ backend/models/crypto-community-subscribers.js
@@ -34,7 +34,7 @@ const CryptoCommunitySubscribers = sequelize.define(
     },
     paymentCurrency: {
       type: DataTypes.STRING(255),
-      allowNull: false,
+      allowNull: true,
     },
     dateAdded: {
       type: DataTypes.STRING(255),
