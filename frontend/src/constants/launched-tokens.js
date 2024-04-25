9e8d9ccb3e312f6b85d5cb6201a981aa72a0b861
Mark Friday <davidmarkfriday16@gmail.com>
2024-04-25 17:57:01 +0100

    Launched SolBitcoin token

--- frontend/src/constants/launched-tokens.js
+++ frontend/src/constants/launched-tokens.js
@@ -1,7 +1,18 @@
 import rozyCoin from "../assets/launched-tokens/rozycoin.jpg";
 import catWifPod from "../assets/launched-tokens/catwifpod.webp";
+import solBTC from "../assets/launched-tokens/solbtc.jpg";
 
 export const launchedTokens = {
+  SolBTCToken: {
+    id: 3,
+    tokenName: "SolBitcoin",
+    ticker: "$SOLBTC",
+    tokenImage: solBTC,
+    description: `SolBitcoin is a utility token by Benarbitrage. $SOLBTC is set to be used for digital financial transactions within Benarbitrage and other platforms, thus increasing it's value and utility.`,
+    contractAddress: "",
+    dateLaunched: "25/4/2024 6:05PM",
+    isMainToken: true,
+  },
   CatWifPodToken: {
     id: 2,
     tokenName: "CatWifPod",
@@ -9,8 +20,7 @@ export const launchedTokens = {
     tokenImage: catWifPod,
     description: `$CATPOD is a meme coin with utility and operates on the Solana Blockchain. Soon, you will be able to carry out financial transactions on Benarbitrage using $CATPOD.`,
     contractAddress: "",
-    dateLaunched: "24/4/2024 10PM",
-    isMainToken: true,
+    dateLaunched: "24/4/2024 10:15PM",
   },
   RozyCoinToken: {
     id: 1,
